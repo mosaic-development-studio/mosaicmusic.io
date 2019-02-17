87b8a508c1fc5a67b1ccd29bed6141921dc6fd5f
Mike Skalandunas <mskalandunas@gmail.com>
2019-02-16 18:28:41 -0800

    [29]: add above-the-fold styles

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    rootDir: './',
-    testMatch: [
-        '<rootDir>/__test__/**/*.test.spec.js',
-        '<rootDir>/__test__/**/*.test.snap.js',
-        '<rootDir>/__test__/**/*.test.unit.js'
-    ]
-};
